d594bb67a47dc2103913eff3a7b17c930df6e173
Nexa-pay <stellargamingg99@gmail.com>
2026-02-20 18:34:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import yt_dlp
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import AudioPiped
+from pytgcalls.types.input_stream import InputAudioStream, InputStream
+from pytgcalls.types.input_stream.quality import HighQualityAudio
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
@@ -16,7 +17,11 @@ user = TelegramClient(StringSession(STRING), API_ID, API_HASH)
 vc = PyTgCalls(user)
 
 def download(query):
-    ydl_opts = {"format": "bestaudio","outtmpl": "song.%(ext)s","quiet": True}
+    ydl_opts = {
+        "format": "bestaudio",
+        "outtmpl": "song.%(ext)s",
+        "quiet": True
+    }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(f"ytsearch:{query}", download=True)["entries"][0]
         return "song.webm", info["title"]
@@ -24,7 +29,14 @@ def download(query):
 @bot.on(events.NewMessage(pattern=r"/play (.+)"))
 async def play(event):
     file, title = download(event.pattern_match.group(1))
-    await vc.join_group_call(event.chat_id, AudioPiped(file))
+
+    await vc.join_group_call(
+        event.chat_id,
+        InputStream(
+            InputAudioStream(file, HighQualityAudio())
+        )
+    )
+
     await event.reply(f"▶️ Playing: {title}")
 
 async def main():
